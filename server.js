892b869472c6b9cf5c9bd1309d3a2512ee0a61e3
Lindsay Lopez <lopez95@berkeley.edu>
2020-02-18 22:05:47 -0800

    Time to check if the tests pass!

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ var express = require('express');
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const validUrl = require('valid-url');
+const shortid = require('shortid');
 require('dotenv').config();
 
 var cors = require('cors');
 
 var app = express();
 
-// Basic Configuration 
-// var port = process.env.PORT || 3000;
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/public', express.static(process.cwd() + '/public'));
 
 /** this project needs a db !! **/ 
 mongoose.connect(process.env.MONGOLAB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -42,17 +50,37 @@ process.on('SIGINT', () => {
   });
 });
 
-app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-app.use('/public', express.static(process.cwd() + '/public'));
-
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: String
+});
+
+const Url = mongoose.model('Url', urlSchema);
+
+app.post('/api/shorturl/new', (req, res) => {
+  let original_url = req.body.url;
+  if (!validUrl.isUri(original_url)) {
+    res.send({"error":"invalid URL"});
+  } else {
+    let short_url = shortid.generate();
+    let urls = new Url({original_url, short_url});
+    urls.save(err => {
+      if (err) return console.log(err);
+      res.send({original_url, short_url});
+    });
+  }
+});
+
+app.get('/api/shorturl/:shorturl', (req, res) => {
+  let short_url = req.params.shorturl;
+  db.collection('urls').findOne({short_url}, (err, data) => {
+    if (err) return console.log(err);
+    res.redirect(data.original_url);
+  });
 });
 
   
